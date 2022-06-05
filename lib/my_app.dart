ddb20327716c98e9697d920b2605b9b1fd60378d
PilKvas <hm10bvb@mail.ru>
2022-06-05 16:43:50 +0300

    analyzer fix

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,15 +1,6 @@
 // ignore_for_file: file_names
-
 import 'package:flutter/material.dart';
 import 'package:flutter_solar_system_application/main_navigation.dart';
-import 'package:flutter_solar_system_application/planets/planet_earth_screen.dart';
-import 'package:flutter_solar_system_application/planets/planet_jupiter_screen.dart';
-import 'package:flutter_solar_system_application/planets/planet_mars_screen.dart';
-import 'package:flutter_solar_system_application/planets/planet_mercury_screen.dart';
-import 'package:flutter_solar_system_application/planets/planet_neptune_screen.dart';
-import 'package:flutter_solar_system_application/planets/planet_saturn_screen.dart';
-import 'package:flutter_solar_system_application/planets/planet_uranus_screen.dart.dart';
-import 'package:flutter_solar_system_application/planets/planet_venus_screen.dart';
 
 class MyApp extends StatelessWidget {
 
