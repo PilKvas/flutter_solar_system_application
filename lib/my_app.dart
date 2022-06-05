06f9caa209d75af8b80cd660ed4df68c1db1c966
PilKvas <hm10bvb@mail.ru>
2022-06-05 16:20:04 +0300

    navigation refactoring

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:flutter_solar_system_application/main_navigation.dart';
 import 'package:flutter_solar_system_application/planets/planet_earth_screen.dart';
 import 'package:flutter_solar_system_application/planets/planet_jupiter_screen.dart';
 import 'package:flutter_solar_system_application/planets/planet_mars_screen.dart';
@@ -11,24 +12,17 @@ import 'package:flutter_solar_system_application/planets/planet_uranus_screen.da
 import 'package:flutter_solar_system_application/planets/planet_venus_screen.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
+  static final mainNavigation = MainNavigation();
+
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Solar System',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const PlanetMercuryScreen(),
-        '/planet_venus_screen': (context) => const PlanetVenusScreen(),
-        '/planet_earth_screen': (context) => const PlanetEarthScreen(),
-        '/planet_mars_screen': (context) => const PlanetMarsScreen(),
-        '/planet_jupiter_screen': (context) => const PlanetJupiterScreen(),
-        '/planet_saturn_screen': (context) => const PlanetSaturnScreen(),
-        '/planet_uranus_screen': (context) => const PlanetUranusScreen(),
-        '/planet_neptune_screen': (context) => const PlanetNeptuneScreen(),
-      },
+      initialRoute: mainNavigation.initialRoute,
+      routes: mainNavigation.routes,
     );
   }
 }
