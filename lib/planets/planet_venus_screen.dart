e986c1696470963c468966aa2873b351af557cb2
PilKvas <hm10bvb@mail.ru>
2022-07-18 23:24:51 +0300

    data_json

--- lib/planets/planet_venus_screen.dart
+++ lib/planets/planet_venus_screen.dart
@@ -1,247 +1,41 @@
 import "package:flutter/material.dart";
-import 'package:flutter_solar_system_application/configuration/app_assets.dart';
 import 'package:flutter_solar_system_application/configuration/app_colors.dart';
-import 'package:flutter_solar_system_application/widgets/background.dart';
-import 'package:flutter_solar_system_application/widgets/drawer_navigation.dart';
-import 'package:flutter_solar_system_application/widgets/planet_info.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:flutter_solar_system_application/configuration/app_assets.dart';
+import 'package:flutter_solar_system_application/planets/planet_screen.dart';
+import 'package:flutter_solar_system_application/widgets/data_json.dart';
+import 'package:flutter_solar_system_application/widgets/planet_data.dart';
 
-class PlanetVenusScreen extends StatefulWidget {
+/* Является 'строительныи лесом' где расположены все собственноручно написанные виджеты , ответственность за функциональную часть кнопок, скролл, хранит в себе все переменные виджетов, обработка нажатий по кнопкам, отображение данных плаенты Земля, Логика отображения, Стилизация*/
+
+class PlanetVenusScreen extends StatelessWidget {
+  final queue = 2;
   const PlanetVenusScreen({Key? key}) : super(key: key);
-
-  @override
-  _PlanetVenusScreenState createState() => _PlanetVenusScreenState();
-}
-
-class _PlanetVenusScreenState extends State<PlanetVenusScreen> {
-  String mainInfo = AppAssets.venusMainInfoOverview;
-
-  String mainSvgAsset = AppAssets.venusOverviewAsset;
-
-  final String secondarySvgAsset = AppAssets.venusSurfaceAsset;
-
-  final Color buttonBackgroundColor = AppColors.venusColor;
-
-  final Color borderInfoColor = AppColors.venusColor;
-
-  final String mainTitle = AppAssets.venusTitle;
-
-  static const Size mainSizeSvgAsset = Size.square(256);
-
-  bool isActiveOverview = true;
-
-  bool isActiveStructure = false;
-
-  bool isActiveSurface = false;
-
-  void overviewButton() {
-    setState(() {
-      mainSvgAsset = AppAssets.venusOverviewAsset;
-      mainInfo = AppAssets.venusMainInfoOverview;
-      isActiveOverview = true;
-      isActiveStructure = false;
-      isActiveSurface = false;
-    });
-  }
-
-  void structureButton() {
-    setState(() {
-      mainSvgAsset = AppAssets.venusStructureAsset;
-      mainInfo = AppAssets.venusMainInfoStructure;
-      isActiveOverview = false;
-      isActiveStructure = true;
-      isActiveSurface = false;
-    });
-  }
-
-  void surfaceButton() {
-    setState(() {
-      mainSvgAsset = AppAssets.venusOverviewAsset;
-      mainInfo = AppAssets.venusMainInfoSurface;
-      isActiveOverview = false;
-      isActiveStructure = false;
-      isActiveSurface = true;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      endDrawer: const DrawerNavigation(),
-      appBar: AppBar(
-        backgroundColor: AppColors.background,
-        title: Text(
-          'THE PLANETS',
-          style: GoogleFonts.antonio(
-              textStyle: const TextStyle(
-                  color: AppColors.white,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 28,
-                  letterSpacing: -1.05)),
-        ),
-      ),
-      body: Background(
-        child: SingleChildScrollView(
-          child: SizedBox(
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  decoration: const BoxDecoration(
-                    border: Border(
-                        bottom: BorderSide(color: AppColors.borderColor),
-                        top: BorderSide(color: AppColors.borderColor)),
-                  ),
-                  child: ButtonBar(
-                    alignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      TextButton(
-                        onPressed: overviewButton,
-                        style: ButtonStyle(
-                          backgroundColor: isActiveOverview
-                              ? MaterialStateProperty.all(buttonBackgroundColor)
-                              : MaterialStateProperty.all(null),
-                          padding: MaterialStateProperty.all(
-                              const EdgeInsets.all(10.0)),
-                        ),
-                        child: Text('OVERVIEW',
-                            style: GoogleFonts.leagueSpartan(
-                              textStyle: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                fontSize: 11,
-                                letterSpacing: 1.96,
-                                color: isActiveOverview
-                                    ? AppColors.white
-                                    : AppColors.lightGrey,
-                              ),
-                            )),
-                      ),
-                      TextButton(
-                          style: ButtonStyle(
-                            backgroundColor: isActiveStructure
-                                ? MaterialStateProperty.all(
-                                    buttonBackgroundColor)
-                                : MaterialStateProperty.all(null),
-                            padding: MaterialStateProperty.all(
-                                const EdgeInsets.all(10.0)),
-                          ),
-                          onPressed: structureButton,
-                          child: Text(
-                            'STRUCTURE',
-                            style: GoogleFonts.leagueSpartan(
-                              textStyle: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                fontSize: 11,
-                                letterSpacing: 1.96,
-                                color: isActiveStructure
-                                    ? AppColors.white
-                                    : AppColors.lightGrey,
-                              ),
-                            ),
-                          )),
-                      TextButton(
-                        style: ButtonStyle(
-                          backgroundColor: isActiveSurface
-                              ? MaterialStateProperty.all(buttonBackgroundColor)
-                              : MaterialStateProperty.all(null),
-                          padding: MaterialStateProperty.all(
-                              const EdgeInsets.all(10.0)),
-                        ),
-                        onPressed: surfaceButton,
-                        child: Text(
-                          'SURFACE',
-                          style: GoogleFonts.leagueSpartan(
-                            textStyle: TextStyle(
-                              fontWeight: FontWeight.w700,
-                              fontSize: 11,
-                              letterSpacing: 1.96,
-                              color: isActiveSurface
-                                  ? AppColors.white
-                                  : AppColors.lightGrey,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 65,
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 35),
-                  child: Column(
-                    children: [
-                      Stack(
-                        children: [
-                          SvgPicture.asset(
-                            mainSvgAsset,
-                            width: mainSizeSvgAsset.width,
-                            height: mainSizeSvgAsset.height,
-                          ),
-                          Opacity(
-                            opacity: isActiveSurface ? 1 : 0,
-                            child: Image.asset(
-                              secondarySvgAsset,
-                              width: mainSizeSvgAsset.width,
-                              height: mainSizeSvgAsset.height,
-                            ),
-                          )
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 90,
-                      ),
-                      Text(mainTitle,
-                          style: GoogleFonts.antonio(
-                            textStyle: const TextStyle(
-                                fontSize: 40,
-                                fontWeight: FontWeight.w400,
-                                color: AppColors.white),
-                          )),
-                      const SizedBox(height: 40),
-                      Text(mainInfo,
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.leagueSpartan(
-                            textStyle: const TextStyle(
-                                fontSize: 17,
-                                color: AppColors.whiteBody,
-                                fontWeight: FontWeight.w300,
-                                height: 1.3),
-                          )),
-                      const SizedBox(
-                        height: 40,
-                      ),
-                      PlanetInfo(
-                        leftSideText: 'ROTATION TIME',
-                        rightSideText: '58.6 DAYS',
-                        borderInfoColor: borderInfoColor,
-                      ),
-                      PlanetInfo(
-                        leftSideText: 'REVOLUTION TIME',
-                        rightSideText: '87.97 DAYS',
-                        borderInfoColor: borderInfoColor,
-                      ),
-                      PlanetInfo(
-                        leftSideText: 'RADIUS',
-                        rightSideText: '2,439.7 KM',
-                        borderInfoColor: borderInfoColor,
-                      ),
-                      PlanetInfo(
-                        leftSideText: 'AVERAGE TEMP.',
-                        rightSideText: '430°C',
-                        borderInfoColor: borderInfoColor,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+    return PlanetScreen(
+        data: PlanetData(
+          title: planetAsset[queue]['name'],
+          overview: PlanetInfoData(
+            info: planetAsset[queue]['overview']['content'],
+            asset: planetAsset[queue]['images']['planet'],
           ),
+          // url: Uri.parse(planetAsset[2]['overview']['source'])),
+          structure: PlanetInfoData(
+            info: planetAsset[queue]['structure']['content'],
+            asset: planetAsset[queue]['images']['internal'],
+          ),
+          // url: Uri.parse('https://en.wikipedia.org/wiki/Earth#Internal_structure')),
+          surface: PlanetInfoData(
+            info: planetAsset[queue]['geology']['content'],
+            asset: planetAsset[queue]['images']['geology'],
+            // url: Uri.parse('https://en.wikipedia.org/wiki/Earth#Surface')),
+          ),
+          rotationTime: planetAsset[queue]['rotation'],
+          revolutionTime: planetAsset[queue]['revolution'],
+          radius: planetAsset[queue]['radius'],
+          averageTemperature: planetAsset[queue]['temperature'],
         ),
-      ),
-    );
+        buttonBackgroundColor: AppColors.earthColor,
+        borderInfoColor: AppColors.earthColor);
   }
 }
